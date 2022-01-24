55480094e51c13a9b719f6a5e7a330876b85da0e
Derzhko <alxDerzhko@yandex.ru>
2022-01-24 14:57:07 +0300

    Add comments first try

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
 const list = document.querySelector('.comments-list');
 
+const form = document.querySelector('form');
+
 function parseDate(date){
+    console.log(date);
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear()
 
     return `${day}.${month}.${year}`
-}
-
-
-function createCommentMarkup({name, rating, text, date}) {
-    let comment = document.createElement('div');
-    comment.classList.add('comment');
-    comment.innerHTML = ` <div class="comment__title">
-                <p class="comment__person-name">${name}</p>
-                <div class="comment__rating">
-                    ${rating}/5
-                </div>
-            </div>
-            <div class="comment__text">
-                <p>${text}</p>
-            </div>
-            <div class="comment__date">
-                <p>${parseDate(date)}</p>
-            </div>`
-
-    return comment
 }
 
 const comments = [
@@ -59,6 +42,49 @@ const comments = [
     }
 ]
 
+function createCommentMarkup({name, rating, text, date}) {
+    let comment = document.createElement('div');
+    comment.classList.add('comment');
+    comment.innerHTML = ` <div class="comment__title">
+                <p class="comment__person-name">${name}</p>
+                <div class="comment__rating">
+                    ${rating}/5
+                </div>
+            </div>
+            <div class="comment__text">
+                <p>${text}</p>
+            </div>
+            <div class="comment__date">
+                <p>${parseDate(date)}</p>
+            </div>`
+
+    return comment
+}
+
+
+function addComment(){
+    comments.forEach((comment) => {
+        const markup = createCommentMarkup(comment);
+        list.append(markup);
+    })
+}
+
+
+
+function formSubmit(event){
+    event.preventDefault();
+    let comment = {
+        name: event.target[0].value,
+        rating: event.target[2].value,
+        text: event.target[1].value,
+        date: new Date(),
+    }
+    comments.push(comment);
+    console.log(comments);
+    const markup = createCommentMarkup(comment);
+    list.append(markup);
+    form.reset();
+}
 // for (let elem of comments){
 //     console.log(elem)
 // }
@@ -67,3 +93,5 @@ comments.forEach((comment) => {
     const markup = createCommentMarkup(comment);
     list.append(markup);
 })
+
+form.addEventListener('submit', (event) => {formSubmit(event)})
